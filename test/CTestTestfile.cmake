a526fc39b351cf222af6b6ddedbeccec710dde2d
Ulf Kargén <ulf.kargen@liu.se>
2021-09-09 18:17:20 +0200

    Fixed tests.

---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ulf/work/desync-project/desync-cc/test
+# Build directory: /home/ulf/work/desync-project/desync-cc/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-basic1 "make" "check" "CC=\"/home/ulf/work/desync-project/desync-cc/bin/desync-cc\"")
+set_tests_properties(test-basic1 PROPERTIES  WORKING_DIRECTORY "/home/ulf/work/desync-project/desync-cc/test/basic1" _BACKTRACE_TRIPLES "/home/ulf/work/desync-project/desync-cc/test/CMakeLists.txt;5;add_test;/home/ulf/work/desync-project/desync-cc/test/CMakeLists.txt;0;")
+add_test(test-basic2 "make" "check" "CC=\"/home/ulf/work/desync-project/desync-cc/bin/desync-cc\"")
+set_tests_properties(test-basic2 PROPERTIES  WORKING_DIRECTORY "/home/ulf/work/desync-project/desync-cc/test/basic2" _BACKTRACE_TRIPLES "/home/ulf/work/desync-project/desync-cc/test/CMakeLists.txt;11;add_test;/home/ulf/work/desync-project/desync-cc/test/CMakeLists.txt;0;")
